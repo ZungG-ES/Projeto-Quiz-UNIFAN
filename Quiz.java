c0704a61b0c38e0295352847dd087fe7059bae27
Adrian Rezende da Silva <rezendeadrian007@gmail.com>
2024-11-12 19:22:49 -0300

    Adicionado o numeros das questoes

--- Quiz.java
+++ Quiz.java
@@ -16,7 +16,7 @@ public class Quiz {
 
         // Questão 1
         questoes[0] = new Questao();
-        questoes[0].pergunta = "Qual é o nome completo de Alvo Dumbledore?";
+        questoes[0].pergunta = "1 - Qual é o nome completo de Alvo Dumbledore?";
         questoes[0].opcaoA = "A) Alvo Percival Wulfrico Brian Dumbledore";
         questoes[0].opcaoB = "B) Alvo Brian Dumbledore";
         questoes[0].opcaoC = "C) Alvo Percival Dumbledore";
@@ -26,7 +26,7 @@ public class Quiz {
 
         // Questão 2
         questoes[1] = new Questao();
-        questoes[1].pergunta = "Qual é a casa de Hogwarts de Harry Potter?";
+        questoes[1].pergunta = "2 - Qual é a casa de Hogwarts de Harry Potter?";
         questoes[1].opcaoA = "A) Sonserina";
         questoes[1].opcaoB = "B) Lufa-Lufa";
         questoes[1].opcaoC = "C) Corvinal";
@@ -36,7 +36,7 @@ public class Quiz {
 
         // Questão 3
         questoes[2] = new Questao();
-        questoes[2].pergunta = "Qual é o nome do elfo doméstico de Harry Potter?";
+        questoes[2].pergunta = "3 - Qual é o nome do elfo doméstico de Harry Potter?";
         questoes[2].opcaoA = "A) Dobby";
         questoes[2].opcaoB = "B) Kreacher";
         questoes[2].opcaoC = "C) Winky";
@@ -46,7 +46,7 @@ public class Quiz {
 
         // Questão 4
         questoes[3] = new Questao();
-        questoes[3].pergunta = "Qual é o patrono de Hermione Granger?";
+        questoes[3].pergunta = "4 - Qual é o patrono de Hermione Granger?";
         questoes[3].opcaoA = "A) Lontra";
         questoes[3].opcaoB = "B) Cervo";
         questoes[3].opcaoC = "C) Lebre";
@@ -56,7 +56,7 @@ public class Quiz {
 
         // Questão 5
         questoes[4] = new Questao();
-        questoes[4].pergunta = "Qual é a posição de Harry Potter no time de Quadribol?";
+        questoes[4].pergunta = "5 - Qual é a posição de Harry Potter no time de Quadribol?";
         questoes[4].opcaoA = "A) Batedor";
         questoes[4].opcaoB = "B) Apanhador";
         questoes[4].opcaoC = "C) Artilheiro";
@@ -66,7 +66,7 @@ public class Quiz {
 
         // Questão 6
         questoes[5] = new Questao();
-        questoes[5].pergunta = "Qual é o nome do pai de Draco Malfoy?";
+        questoes[5].pergunta = "6 - Qual é o nome do pai de Draco Malfoy?";
         questoes[5].opcaoA = "A) Lucius Malfoy";
         questoes[5].opcaoB = "B) Severus Snape";
         questoes[5].opcaoC = "C) Sirius Black";
@@ -76,7 +76,7 @@ public class Quiz {
 
         // Questão 7
         questoes[6] = new Questao();
-        questoes[6].pergunta = "Qual é o nome do professor de Poções no primeiro ano de Harry?";
+        questoes[6].pergunta = "7 - Qual é o nome do professor de Poções no primeiro ano de Harry?";
         questoes[6].opcaoA = "A) Minerva McGonagall";
         questoes[6].opcaoB = "B) Filius Flitwick";
         questoes[6].opcaoC = "C) Severus Snape";
@@ -86,7 +86,7 @@ public class Quiz {
 
         // Questão 8
         questoes[7] = new Questao();
-        questoes[7].pergunta = "Qual é o nome do mapa que mostra todos os segredos de Hogwarts?";
+        questoes[7].pergunta = "8 - Qual é o nome do mapa que mostra todos os segredos de Hogwarts?";
         questoes[7].opcaoA = "A) Mapa do Maroto";
         questoes[7].opcaoB = "B) Mapa do Peregrino";
         questoes[7].opcaoC = "C) Mapa do Explorador";
@@ -96,7 +96,7 @@ public class Quiz {
 
         // Questão 9
         questoes[8] = new Questao();
-        questoes[8].pergunta = "Qual é o nome do banco dos bruxos?";
+        questoes[8].pergunta = "9 - Qual é o nome do banco dos bruxos?";
         questoes[8].opcaoA = "A) Gringotes";
         questoes[8].opcaoB = "B) Banco de Hogwarts";
         questoes[8].opcaoC = "C) Banco de Hogsmeade";
@@ -106,7 +106,7 @@ public class Quiz {
 
         // Questão 10
         questoes[9] = new Questao();
-        questoes[9].pergunta = "Qual é o nome do hipogrifo de Hagrid?";
+        questoes[9].pergunta = "10 - Qual é o nome do hipogrifo de Hagrid?";
         questoes[9].opcaoA = "A) Bicuço";
         questoes[9].opcaoB = "B) Norberto";
         questoes[9].opcaoC = "C) Aragog";
@@ -116,7 +116,7 @@ public class Quiz {
 
         // Questão 11
         questoes[10] = new Questao();
-        questoes[10].pergunta = "Qual é o nome do jornal dos bruxos?";
+        questoes[10].pergunta = "11 - Qual é o nome do jornal dos bruxos?";
         questoes[10].opcaoA = "A) O Profeta Diário";
         questoes[10].opcaoB = "B) O Correio Coruja";
         questoes[10].opcaoC = "C) O Diário de Hogwarts";
@@ -126,7 +126,7 @@ public class Quiz {
 
         // Questão 12
         questoes[11] = new Questao();
-        questoes[11].pergunta = "Qual é o nome do feitiço para desarmar o oponente?";
+        questoes[11].pergunta = "12 - Qual é o nome do feitiço para desarmar o oponente?";
         questoes[11].opcaoA = "A) Expelliarmus";
         questoes[11].opcaoB = "B) Stupefy";
         questoes[11].opcaoC = "C) Avada Kedavra";
@@ -136,7 +136,7 @@ public class Quiz {
 
         // Questão 13
         questoes[12] = new Questao();
-        questoes[12].pergunta = "Qual é o nome do feitiço que conjura um patrono?";
+        questoes[12].pergunta = "13 - Qual é o nome do feitiço que conjura um patrono?";
         questoes[12].opcaoA = "A) Expecto Patronum";
         questoes[12].opcaoB = "B) Lumos";
         questoes[12].opcaoC = "C) Wingardium Leviosa";
@@ -146,7 +146,7 @@ public class Quiz {
 
         // Questão 14
         questoes[13] = new Questao();
-        questoes[13].pergunta = "Qual é o nome do feitiço que abre portas trancadas?";
+        questoes[13].pergunta = "14 - Qual é o nome do feitiço que abre portas trancadas?";
         questoes[13].opcaoA = "A) Alohomora";
         questoes[13].opcaoB = "B) Lumos";
         questoes[13].opcaoC = "C) Wingardium Leviosa";
@@ -156,7 +156,7 @@ public class Quiz {
 
         // Questão 15
         questoes[14] = new Questao();
-        questoes[14].pergunta = "Qual é o nome do feitiço que ilumina a ponta da varinha?";
+        questoes[14].pergunta = "15 - Qual é o nome do feitiço que ilumina a ponta da varinha?";
         questoes[14].opcaoA = "A) Lumos";
         questoes[14].opcaoB = "B) Nox";
         questoes[14].opcaoC = "C) Wingardium Leviosa";
